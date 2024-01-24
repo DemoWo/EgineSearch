a245c426004c19d2cece92b38adc7b5e8065754d
Demon <v1entro@ya.ru>
2024-01-24 19:27:55 +0300

    24-01-24-2

---
+++ search_engine/src/converterjson.cpp
@@ -0,0 +1,21 @@
+#include "converterjson.h"
+
+std::vector<std::string> ConverterJSON::GetRequests()
+{
+
+}
+
+int ConverterJSON::GetResponsesLimit()
+{
+    return 10;
+}
+
+std::vector<std::string> ConverterJSON::GetTextDocuments()
+{
+
+}
+
+void ConverterJSON::putAnswers( std::vector<std::vector<std::pair<int, float> > > answers )
+{
+
+}
---
+++ search_engine/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "converterjson.h"
+
+#include "gtest/gtest.h"
+//TEST(sample_test_case, sample_test)
+//{
+//    EXPECT_EQ(1, 1);
+//}
+
+int main() {
+    int a = ConverterJSON::GetResponsesLimit();
+    std::cout << a << std::endl;
+    return 0;
+}
